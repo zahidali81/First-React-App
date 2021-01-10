7305062a620a30aaebe6404cebec325e259c5d8d
zahidali81 <zahid.alia380@gmail.com>
2021-01-10 17:43:00 +0000

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer(){
+
+return (
+    <footer>
+        <p>copyright © {new Date().getFullYear()}</p>
+    </footer>
+)
+    
+}
+
+export default Footer;
+
+
+//4. Create a Footer.jsx component that renders a <footer> element
+//to show a copyright message in a <p> with a dynamically updated year.
---
+++ src/components/Note.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Note(){
+return (
+        <div className = "note">
+            <h1>Happy hacking!</h1>
+            <p>Code well!</p>
+        </div>
+)
+}
+
+
+
+
+export default Note;
+//5. Create a Note.jsx component to show a <div> element with a
+//<h1> for a title and a <p> for the content.
